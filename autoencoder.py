fbc1c317b53afa3b1d1bc4dd76bcdbb6020e982e
Alejandro Queiruga <afqueiruga@lbl.gov>
2018-09-18 13:24:25 -0700

    Working on that new autoencoder

--- autoencoder.py
+++ autoencoder.py
@@ -12,23 +12,23 @@ class Autoencoder(object):
     def __init__(self, size_x, size_q, data):
         self.size_x = size_x
         self.size_q = size_q
-
+        self.dtype = tf.float32
         # Make the trainer
-        self.train_step = self.make_train_step(data)
+        self.train_step = self._make_train_step(data)
         # Make evaluating graphs
-        self.i_x = tf.placeholder( (size_x,), dtype=tf.float32 )
-        self.o_q = encode( self.i_x )
-        self.i_q = tf.placeholder( (size_q,), dtype=tf.float32 )
-        self.o_x = decode( self.i_q )
+        self.i_x = tf.placeholder( shape=(None,size_x,), dtype=tf.float32 )
+        self.o_q = self.encode( self.i_x )
+        self.i_q = tf.placeholder( shape=(None,size_q,), dtype=tf.float32 )
+        self.o_x = self.decode( self.i_q )
         # Make the loggers for tensorboard
         
     def encode(self, x):
-        W = tf.Variable( (self.size_q, self.size_x) )
-        b = tf.Variable( (self.size_q,) )
+        W = self._var( (self.size_q, self.size_x) )
+        b = self._var( (self.size_q,) )
         return tf.matmul(W,x)+b
     def decode(self, q):
-        W = tf.Variable( (self.size_x, self.size_q) )
-        b = tf.Variable( (self.size_x,) )
+        W = self._var( (self.size_x, self.size_q) )
+        b = self._var( (self.size_x,) )
         return tf.matmul(W,q)+b
     def goal(self, data):
         pred = self.decode(self.encode(data))
@@ -40,25 +40,29 @@ class Autoencoder(object):
         return ts
     def eval_q(self, i_x):
         return sess.eval( self.o_q, feed_dict={self.i_x:i_x} )
+
+    def _var(self, shape, stddev=0.1):
+        return tf.Variable(tf.truncated_normal(shape=shape, stddev=stddev),
+                               dtype=self.dtype)
     
 class PolyAutoencoder(Autoencoder):
     """
     The basic implementation.
     """
-    def __init__(self, size_x, size_q, data, p_enc, p_dec):
+    def __init__(self, size_x, size_q, data, Np_enc, Np_dec):
         self.Np_enc = Np_enc
         self.Np_dec = Np_dec
         Autoencoder.__init__(self,size_x, size_q, data)
     def encode(self, x):
         N_coeff = atu.Npolyexpand( self.size_x, self.Np_enc )
-        We1 = tf.Variable( (self.size_q, N_coeff) )
-        be1 = tf.Variable( (self.size_q,) )
-        return tf.matmul( We1, atu.polyexpand(x, self.Np_enc) ) + be1
+        We1 = self._var( (N_coeff, self.size_q) )
+        be1 = self._var( (self.size_q,) )
+        return tf.matmul( atu.polyexpand(x, self.Np_enc), We1 ) + be1
     def decode(self, q):
         N_coeff = atu.Npolyexpand( self.size_q, self.Np_dec )
-        We1 = tf.Variable( (self.size_x, N_coeff) )
-        be1 = tf.Variable( (self.size_x,) )
-        return tf.matmul( We1, atu.polyexpand(q, self.Np_dec) ) + be1
+        We1 = self._var( (N_coeff, self.size_x) )
+        be1 = self._var( (self.size_x,) )
+        return tf.matmul( atu.polyexpand(q, self.Np_dec), We1 ) + be1
     
 class ClassifyingPolyAutoencoder(Autoencoder):
     """
