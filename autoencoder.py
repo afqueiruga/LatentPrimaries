31aaa1fa3568bc289e8e2a5579dbdc6214f25711
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2018-12-26 02:00:26 -0500

    Editing the autoencoder

--- autoencoder.py
+++ autoencoder.py
@@ -33,13 +33,13 @@ class Autoencoder(object):
         # Make the loggers for tensorboard
         
     def encode(self, x):
-        W = self._var( "enc:W", (self.size_q, self.size_x) )
-        b = self._var( "enc:b", (self.size_q,) )
+        W = self._var( "enc_W", (self.size_q, self.size_x) )
+        b = self._var( "enc_b", (self.size_q,) )
         return tf.matmul(W,x)+b
     
     def decode(self, q):
-        W = self._var( "dec:W", (self.size_x, self.size_q) )
-        b = self._var( "dec:b", (self.size_x,) )
+        W = self._var( "dec_W", (self.size_x, self.size_q) )
+        b = self._var( "dec_b", (self.size_x,) )
         return tf.matmul(W,q)+b
     
     def make_goal(self, data):
@@ -48,7 +48,9 @@ class Autoencoder(object):
         return p
 
     def _make_train_step(self, data):
-        ts = tf.train.AdamOptimizer(1e-2).minimize(self.make_goal(data))
+        opt = tf.train.AdamOptimizer(1e-2)
+        loss = self.make_goal(data)
+        ts = opt.minimize(loss,global_step=tf.train.get_global_step())
         return ts
     
     def eval_q(self, i_x):
@@ -59,7 +61,8 @@ class Autoencoder(object):
             v = self.vars[name]
         except KeyError:
             v = tf.Variable(tf.truncated_normal(shape=shape, stddev=stddev),
-                               dtype=self.dtype)
+                            name=name,
+                            dtype=self.dtype)
             self.vars[name] = v
         return v
     
@@ -75,14 +78,17 @@ class Autoencoder(object):
         plt.plot(x_dec[:,XI],x_dec[:,YI],'+')
         plt.axis('square');
         
-    def save_fit(self, fname, header):
+    def save_fit(self, fname, header,sess=None):
         qs = []
         for j in range(20):
             qs.append(self.o_q.eval(feed_dict={self.i_x:self.data.eval()}))
         qs = np.vstack(qs)
-        xs = self.o_x.eval(feed_dict={self.i_q:qs})
-        from matplotlib import pylab as plt
-        plt.plot(xs[:,0],xs[:,1],',')
+        if sess:
+            xs = sess.run(self.o_x,feed_dict={self.i_q:qs})
+        else:
+            xs = self.o_x.eval(feed_dict={self.i_q:qs})
+#         from matplotlib import pylab as plt
+#         plt.plot(xs[:,0],xs[:,1],',')
         dat = np.hstack([xs,qs])
         np.savetxt(fname,dat,delimiter=", ",header=header,comments="")
 
@@ -98,14 +104,14 @@ class PolyAutoencoder(Autoencoder):
         
     def encode(self, x):
         N_coeff = atu.Npolyexpand( self.size_x, self.Np_enc )
-        We1 = self._var("enc:W", (N_coeff, self.size_q) )
-        be1 = self._var("enc:b", (self.size_q,) )
+        We1 = self._var("enc_W", (N_coeff, self.size_q) )
+        be1 = self._var("enc_b", (self.size_q,) )
         return tf.matmul( atu.polyexpand(x, self.Np_enc), We1 ) + be1
     
     def decode(self, q):
         N_coeff = atu.Npolyexpand( self.size_q, self.Np_dec )
-        We1 = self._var("dec:W", (N_coeff, self.size_x) )
-        be1 = self._var("dec:b", (self.size_x,) )
+        We1 = self._var("dec_W", (N_coeff, self.size_x) )
+        be1 = self._var("dec_b", (self.size_x,) )
         return tf.matmul( atu.polyexpand(q, self.Np_dec), We1 ) + be1
     
 class DeepPolyAutoencoder(Autoencoder):
@@ -123,14 +129,14 @@ class DeepPolyAutoencoder(Autoencoder):
     def encode(self, x):
         N_coeff = atu.Npolyexpand( self.size_x, self.Np_enc )
         for hidden_size in enc_layers + [self.size_q]:
-            We1 = self._var("enc:W", (N_coeff, self.size_q) )
-            be1 = self._var("enc:b", (self.size_q,) )
+            We1 = self._var("enc_W", (N_coeff, self.size_q) )
+            be1 = self._var("enc_b", (self.size_q,) )
         return tf.matmul( atu.polyexpand(x, self.Np_enc), We1 ) + be1
     
     def decode(self, q):
         N_coeff = atu.Npolyexpand( self.size_q, self.Np_dec )
-        We1 = self._var("dec:W", (N_coeff, self.size_x) )
-        be1 = self._var("dec:b", (self.size_x,) )
+        We1 = self._var("dec_W", (N_coeff, self.size_x) )
+        be1 = self._var("dec_b", (self.size_x,) )
         return tf.matmul( atu.polyexpand(q, self.Np_dec), We1 ) + be1
     
 class ClassifyingPolyAutoencoder(Autoencoder):
@@ -155,19 +161,19 @@ class ClassifyingPolyAutoencoder(Autoencoder):
         
     def encode(self, x):
         N_coeff = atu.Npolyexpand( self.size_x, self.Np_enc )
-        W = self._var("enc:W", (N_coeff, self.size_q) )
-        b = self._var("enc:b", (self.size_q,) )
+        W = self._var("enc_W", (N_coeff, self.size_q) )
+        b = self._var("enc_b", (self.size_q,) )
         return tf.matmul( atu.polyexpand(x, self.Np_enc), W ) + b
     
     def classify(self, q):
         qpoly = atu.polyexpand(q, self.Np_dec)
         N_coeff = atu.Npolyexpand( self.size_q, self.Np_dec )
 
-        W2 = self._var("dec:W_bound", (N_coeff, self.N_bound) )
-        b2 = self._var("dec:b_bound", (self.N_bound,) )
+        W2 = self._var("dec_W_bound", (N_coeff, self.N_bound) )
+        b2 = self._var("dec_b_bound", (self.N_bound,) )
         h_bound = tf.nn.relu(tf.tensordot(qpoly,W2,axes=[-1,0])+b2)
         
-        W_select = self._var("dec:W_select", (self.N_bound,self.N_curve))
+        W_select = self._var("dec_W_select", (self.N_bound,self.N_curve))
         h_select = tf.tensordot(h_bound,W_select, axes=[-1,0])
         if self.softmax_it:
             h_select = tf.nn.softmax(h_select)
@@ -177,8 +183,8 @@ class ClassifyingPolyAutoencoder(Autoencoder):
         qpoly = atu.polyexpand(q, self.Np_dec)
         N_coeff = atu.Npolyexpand( self.size_q, self.Np_dec )
         
-        W1 = self._var("dec:W_curve", (N_coeff, self.N_curve, self.size_x) )
-        b1 = self._var("dec:b_curve", (self.N_curve, self.size_x) )
+        W1 = self._var("dec_W_curve", (N_coeff, self.N_curve, self.size_x) )
+        b1 = self._var("dec_b_curve", (self.N_curve, self.size_x) )
         h_curve = tf.tensordot(qpoly,W1,axes=[-1,0])+b1
         
         h_select = self.classify(q)
@@ -187,20 +193,23 @@ class ClassifyingPolyAutoencoder(Autoencoder):
         
         return x
     
-    def save_fit(self, fname, header):
+    def save_fit(self, fname, header,sess=None):
         qs = []
         ixs = []
+        def myev(x,feed_dict={}):
+            if sess:
+                return sess.run(x,feed_dict=feed_dict)
+            else:
+                return x.eval(feed_dict=feed_dict)
         for j in range(20):
-            ix = self.data.eval()
-            qs.append(self.o_q.eval(feed_dict={self.i_x:ix}))
+            ix = myev(self.data)
+            qs.append(myev(self.o_q,feed_dict={self.i_x:ix}))
             ixs.append(ix)
         qs = np.vstack(qs)
         ixs = np.vstack(ixs)
-        oxs = self.o_x.eval(feed_dict={self.i_q:qs})
-        
-        probs = self.o_class.eval(feed_dict={self.i_q:qs})
+        oxs = myev(self.o_x,{self.i_q:qs})
+        probs = myev(self.o_class,feed_dict={self.i_q:qs})
         classes = probs.argmax(axis=-1)
-        
         errors = ((ixs-oxs)**2).sum(axis=-1)
         
         from matplotlib import pylab as plt
