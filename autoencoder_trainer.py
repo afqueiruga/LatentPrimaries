3b76df197d4f3872498bfef42cbdc824a64cbd61
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2019-01-04 11:58:24 -0800

    Made a run hook like I was supposed to

--- autoencoder_trainer.py
+++ autoencoder_trainer.py
@@ -21,42 +21,63 @@ class SaveAtEndHook(tf.train.SessionRunHook):
     def end(self, session):
         # TODO freeze it
         self._saver.save(session, self.fname)
-# class DoStuffHook(self)
+        
+class DoStuffHook(tf.train.SessionRunHook):
+    def __init__(self, freq=500):
+        self.freq = freq
+        self.global_step = tf.train.get_or_create_global_step()
+    def after_run(self,run_context,run_values):
+        stepnum = run_context.session.run(self.global_step)
+        if stepnum%self.freq==self.freq-1:
+            self.func(run_context,run_values)
+    def __call__(self, func):
+        self.func = func
+        return self
+    
 def train_autoencoder(name, dataset, outerdim, innerdim, hyper=default_hyper):
     autoclass = autoencoder_factory[hyper['type']]
-    hyperpath = hyper['type']+'_'+','.join(map(str,hyper['args']))
+    def sanitize(x):
+        return str(x).replace(' ','').replace('[','(').replace(']',')')
+    hyperpath = hyper['type']+'_'+','.join(map(sanitize,hyper['args']))
     training_dir = "training_"+name+"/"+hyperpath
     # Set up the graph from the inputs
-    n_epoch = 4000
-    image_freq = 200
+    n_epoch = 5000
+    image_freq = 1000
 
     graph = tf.Graph()
     with graph.as_default():
         stream = atu.make_datastream(dataset,batch_size=0,buffer_size=1000)
         stream = tf.transpose(stream)
         global_step = tf.train.get_or_create_global_step()
+        onum = tf.Variable(0,name="csv_output_num")
         ae = autoclass(outerdim, innerdim, stream, *hyper['args'])
         init = tf.global_variables_initializer()
         meta_graph_def = tf.train.export_meta_graph(filename=training_dir+"/final_graph.meta")
-
-        # train away
-        loghook = tf.train.SummarySaverHook(summary_op=
-            tf.summary.scalar("goal",ae.goal),save_steps=50,output_dir=training_dir)
+        
+        # Add some more hooks
+        loghook = tf.train.SummarySaverHook(
+            summary_op=tf.summary.scalar("goal",ae.goal),
+            save_steps=50,output_dir=training_dir)
         stophook = tf.train.StopAtStepHook(num_steps=n_epoch)
         saverhook = SaveAtEndHook(training_dir+"/final_variables.ckpt")
+        # Make a closure into a hook
+        @DoStuffHook(freq=500)
+        def extrahook(ctx,run_values):
+            stepnum = global_step.eval(session=ctx.session)
+            onum_val = onum.eval(session=ctx.session)
+            onum_val+=1
+            onum.load(onum_val, session=ctx.session)
+            print(ae.goal.eval(session=ctx.session))
+            header="T, p, rho, h"
+            ae.save_fit(training_dir+"/surf_{0}.csv".format(onum_val),
+                        header,sess=ctx.session)
+        # set up the session
         session = tf.train.MonitoredTrainingSession(
             checkpoint_dir=training_dir,
-            hooks=[loghook,stophook,saverhook])
+            hooks=[loghook,stophook,saverhook,extrahook])
+        # train away
         with session as sess:
-#             sess.run(init)
             while not sess.should_stop():
-                stepnum = global_step.eval(session=sess)
-                if stepnum%image_freq==image_freq-1:
-                    onum = stepnum/image_freq
-                    print(ae.goal.eval(session=sess))
-                    header="T, p, rho, h"
-                    ae.save_fit(training_dir+"/surf_{0}.csv".format(onum),
-                                header,sess=sess)
                 sess.run(ae.train_step)
     
     
@@ -72,6 +93,7 @@ if __name__=="__main__":
 #         {'type':'Classifying','args':[1,5, 6,12,'sigmoid']},
 #         {'type':'Classifying','args':[1,1, 6,12,'relu']}
         {'type':'Deep','args':[1,[],1,[8,8,8]]},
+#         {'type':'Poly','args':[1,7]},
     ]
     for S in sets_to_try:
         train_autoencoder("water",dataset, 4,2,S)
