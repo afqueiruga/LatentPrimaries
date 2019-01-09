46a896fce74e8353c88dffa7edfaf839cd823e43
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2019-01-09 15:43:04 -0800

    Some writing

---
+++ train_slgc.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import tensorflow as tf
+from autoencoder_trainer import train_autoencoder
+
+training_dir = "/Users/afq/Google Drive/networks/"
+data_dir = "/Users/afq/Dropbox/ML/primaryautoencoder/"
+if __name__=="__main__":
+    dataset = tf.data.experimental.make_csv_dataset(
+        data_dir+'data_files/water_iapw_logp_sharded/*.csv',
+        1000,
+        select_columns=['T','p','rho','h']
+    )
+    sets_to_try = [
+        {'type':'Deep','args':[1,[],1,[12,12,12]]},
+        {'type':'Deep','args':[1,[12],1,[12,12,12]]},
+        {'type':'Poly','args':[2,7]},
+        {'type':'Classifying','args':[1,1, 6,12,'sigmoid']},
+        {'type':'Classifying','args':[2,1, 6,12,'sigmoid']},
+        {'type':'Classifying','args':[2,1, 12,24,'sigmoid']},
+        {'type':'Classifying','args':[1,1, 12,24,'sigmoid']},
+        {'type':'Classifying','args':[1,5, 6,12,'sigmoid']},
+    ]
+    for S in sets_to_try:
+        train_autoencoder("water_slgc_logp",dataset, 4,2,S,
+                         training_dir=training_dir)
