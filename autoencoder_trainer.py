93c31328f5fa2bffc8ec9e9817389cd9f4483377
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2019-05-11 00:02:19 -0700

    refactored so that forking with multiprocessing works

--- autoencoder_trainer.py
+++ autoencoder_trainer.py
@@ -55,7 +55,7 @@ class DoStuffHook(tf.train.SessionRunHook):
     
 def train_autoencoder(name, dataname, outerdim, innerdim, 
                       hyper=default_hyper,
-                      training_dir='',n_epoch=5000, image_freq=1500):
+                      training_dir='',data_dir='',n_epoch=5000, image_freq=1500):
     
     hyperpath = string_identifier(hyper)
     training_dir = training_dir+"/training_"+name+"/"+hyperpath
@@ -69,15 +69,15 @@ def train_autoencoder(name, dataname, outerdim, innerdim,
 #             select_columns=['T',' p',' rho',' h'],
 #             column_defaults=[tf.float64,tf.float64,tf.float64,tf.float64]
 #         )
+# Set up the graph from the inputs
+#         stream = atu.make_datastream(dataset,batch_size=0,buffer_size=1000)
+#         stream = tf.transpose(stream)
         data_all = np.load(data_dir+"water_lg_scaled_train.npy") # TODO
         dataset_all  = tf.data.Dataset.from_tensors(data_all).repeat()
         dataset_mini = tf.data.Dataset.from_tensor_slices(data_all).repeat()
         # test_data = np.load(data_dir+'/'+"water_lg_scaled_test.npy")
         # testset = tf.data.Dataset.from_tensors(test_data).repeat()
-        # Set up the graph from the inputs
         
-#         stream = atu.make_datastream(dataset,batch_size=0,buffer_size=1000)
-#         stream = tf.transpose(stream)
         stream_all = dataset_all.make_one_shot_iterator().get_next()
         stream_mini = dataset_mini.batch(1000).make_one_shot_iterator().get_next()
         global_step = tf.train.get_or_create_global_step()
@@ -121,60 +121,3 @@ def train_autoencoder(name, dataname, outerdim, innerdim,
                 sess.run(ae.train_step)
 
     
-if __name__=="__main__":
-    training_dir = "/Users/afq/Google Drive/networks/"
-    data_dir = "/Users/afq/Dropbox/ML/primaryautoencoder/data_files/"
-#     data_dir = "/Users/afq/Documents/Dropbox/ML/primaryautoencoder/data_files/"
-    dataset = "water_lg_sharded/*.csv"
-    
-    sets_to_try = [
-#         {'type':'Classifying','args':[1,1, 6,12,'tanh']},
-#         {'type':'Classifying','args':[1,1, 6,12,'sigmoid']},
-#         {'type':'Classifying','args':[1,3, 6,12,'tanh'],'ini':'rand','cae':0},
-#         {'type':'Classifying','args':[1,3, 6,12,'tanh'],'ini':'pT','cae':0},
-#         {'type':'Classifying','args':[1,3, 6,12,'tanh'],'ini':'rhoh','cae':0},
-#         {'type':'Classifying','args':[1,3, 6,12,'tanh'],'ini':'rand','cae':1.0},
-#         {'type':'Classifying','args':[1,3, 6,12,'tanh'],'ini':'pT','cae':1.0},
-#         {'type':'Classifying','args':[1,3, 6,12,'tanh'],'ini':'rhoh','cae':1.0},
-#         {'type':'Classifying','args':[1,3, 6,12,'sigmoid'],'ini':'rand','cae':0},
-#         {'type':'Classifying','args':[1,3, 6,12,'sigmoid'],'ini':'pT','cae':0},
-#         {'type':'Classifying','args':[1,3, 6,12,'sigmoid'],'ini':'rhoh','cae':0},
-        {'type':'Classifying','args':[1,3, 6,12,'sigmoid'],'ini':'rand','cae':1.0},
-        {'type':'Classifying','args':[1,3, 6,12,'sigmoid'],'ini':'pT','cae':1.0},
-        {'type':'Classifying','args':[1,3, 6,12,'sigmoid'],'ini':'rhoh','cae':1.0},
-#         {'type':'Classifying','args':[1,4, 6,12,'sigmoid']},
-#         {'type':'Classifying','args':[1,5, 6,12,'sigmoid']},
-#         {'type':'Classifying','args':[1,1, 6,12,'relu']},
-#         {'type':'Deep','args':[1,[],1,[8,8,8]]},
-#         {'type':'Deep','args':[1,[],2,[8,8,8]]},
-#         {'type':'Deep','args':[1,[],3,[8,8,8]]},
-#         {'type':'Poly','args':[1,5],'ini':'rand','cae':0},
-#         {'type':'Poly','args':[1,5],'ini':'pT','cae':0},
-#         {'type':'Poly','args':[1,5],'ini':'rhoh','cae':0},
-#         {'type':'Poly','args':[1,6]},
-#         {'type':'Poly','args':[1,7]},
-    ]
-    inner_penalty = [
-        0.0,
-        0.1,
-    ]
-#     n_epoch = 25000
-    n_epoch = 100
-#     import joblib
-#     @joblib.delayed
-#     def job(S):
-#         train_autoencoder("water_lg",dataset, 4,2,S,
-#                           training_dir=training_dir,
-#                           n_epoch = n_epoch)
-#         return 1
-#     joblib.Parallel(n_jobs=2)(job(S) for S in sets_to_try)
-    import multiprocessing as multi
-    import itertools
-    def job(S):
-        train_autoencoder("water_lg",dataset, 4,2,S,
-                          training_dir=training_dir,
-                          n_epoch = n_epoch)
-#     p = multi.Pool(processes=4)
-#     p.map( job, sets_to_try )
-    for S in sets_to_try:
-        job(S)
