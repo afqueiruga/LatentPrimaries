c27d967ee04ddf8effaa7e321b71fa23c9615ec1
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2019-05-14 19:18:18 -0700

    refactored training parameters and architecture parameters

--- training_water_lg.py
+++ training_water_lg.py
@@ -3,51 +3,27 @@ if __name__=="__main__":
     data_dir = "/Users/afq/Dropbox/ML/primaryautoencoder/data_files/"
 #     data_dir = "/Users/afq/Documents/Dropbox/ML/primaryautoencoder/data_files/"
     dataset = "water_lg_sharded/*.csv"
-    
-    sets_to_try = [
-#         {'type':'Classifying','args':[1,1, 6,12,'tanh']},
-#         {'type':'Classifying','args':[1,1, 6,12,'sigmoid']},
-#         {'type':'Classifying','args':[1,3, 6,12,'tanh'],'ini':'rand','cae':0},
-#         {'type':'Classifying','args':[1,3, 6,12,'tanh'],'ini':'pT','cae':0},
-#         {'type':'Classifying','args':[1,3, 6,12,'tanh'],'ini':'rhoh','cae':0},
-#         {'type':'Classifying','args':[1,3, 6,12,'tanh'],'ini':'rand','cae':1.0},
-#         {'type':'Classifying','args':[1,3, 6,12,'tanh'],'ini':'pT','cae':1.0},
-#         {'type':'Classifying','args':[1,3, 6,12,'tanh'],'ini':'rhoh','cae':1.0},
-        {'type':'Classifying','args':[1,3, 6,12,'sigmoid'],'ini':'rand','cae':0},
-        {'type':'Classifying','args':[1,3, 6,12,'sigmoid'],'ini':'pT','cae':0},
-        {'type':'Classifying','args':[1,3, 6,12,'sigmoid'],'ini':'rhoh','cae':0},
-        {'type':'Classifying','args':[1,3, 6,12,'sigmoid'],'ini':'rand','cae':1.0},
-        {'type':'Classifying','args':[1,3, 6,12,'sigmoid'],'ini':'pT','cae':1.0},
-        {'type':'Classifying','args':[1,3, 6,12,'sigmoid'],'ini':'rhoh','cae':1.0},
-#         {'type':'Classifying','args':[1,4, 6,12,'sigmoid']},
-#         {'type':'Classifying','args':[1,5, 6,12,'sigmoid']},
-#         {'type':'Classifying','args':[1,1, 6,12,'relu']},
-#         {'type':'Deep','args':[1,[],1,[8,8,8]]},
-#         {'type':'Deep','args':[1,[],2,[8,8,8]]},
-#         {'type':'Deep','args':[1,[],3,[8,8,8]]},
-#         {'type':'Poly','args':[1,5],'ini':'rand','cae':0},
-#         {'type':'Poly','args':[1,5],'ini':'pT','cae':0},
-#         {'type':'Poly','args':[1,5],'ini':'rhoh','cae':0},
-#         {'type':'Poly','args':[1,6]},
-#         {'type':'Poly','args':[1,7]},
+    inis = [ 'rand', 'pT', 'rhoh' ]
+    caes = [ 0.0, 1.0 ]
+    extend_train_params = lambda arch : [ dict(ini=ini,cae=cae,**arch) for ini in inis for cae in caes ]
+    archs_to_try = [
+#         {'type':'Classifying','args':[1,3, 6,12,'sigmoid']},
+        {'type':'Classifying','args':[1,1, 6,12,'tanh']},
+        {'type':'Classifying','args':[1,1, 6,12,'sigmoid']},
+        {'type':'Poly','args':[1,4]},
+#         {'type':'Poly','args':[1,5]},
     ]
-    inner_penalty = [
-        0.0,
-        0.1,
-    ]
-#     n_epoch = 25000
+    sets_to_try = []
+    for arch in archs_to_try:
+        sets_to_try.extend(extend_train_params(arch))
+
+
     n_epoch = 1000
-#     import joblib
-#     @joblib.delayed
-#     def job(S):
-#         train_autoencoder("water_lg",dataset, 4,2,S,
-#                           training_dir=training_dir,
-#                           n_epoch = n_epoch)
-#         return 1
-#     joblib.Parallel(n_jobs=2)(job(S) for S in sets_to_try)
+
     import multiprocessing as multi
     import itertools
     def job(S):
+        # Import of tensorflow has to happen *after* forking
         from autoencoder_trainer import train_autoencoder
         train_autoencoder("water_lg",dataset, 4,2,S,
                           training_dir=training_dir,
