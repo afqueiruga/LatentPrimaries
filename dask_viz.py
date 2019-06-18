c0b1267acba4aba1bd9a324b7b4b86d7160512c4
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2019-06-18 12:00:46 -0700

    Got the columns working

--- dask_viz.py
+++ dask_viz.py
@@ -8,8 +8,19 @@ import plotly.graph_objs as go
 import dash_table
 
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://codepen.io/anon/pen/mardKv.css']
 
 app = dash.Dash(__name__)#, external_stylesheets=external_stylesheets)
+app.css.append_css({
+    "external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
+})
+theme =  {
+    'dark': True,
+    'detail': '#007439',
+    'primary': '#00EA64',
+    'secondary': '#6E6E6E',
+}
+
 server = app.server
 app.config.suppress_callback_exceptions = True
 from dash.dependencies import Input, Output, State
@@ -42,12 +53,30 @@ def get_it_all(eos):
 
 all_archs, archs_table, surfs = get_it_all(eos)
 
+##
+# trash
 multi_dropdown = html.Div([html.Div([dcc.Dropdown(id='value-selected', 
                                      options=[{'label':k,'value':k} for k in all_archs],
                                      value=all_archs[0:2], multi=True)],
                        style={"display": "block", "margin-left": "auto", "margin-right": "auto", "width": "60%"})],
              className="row")
 
+@app.callback(
+    [Output('datatable-interactivity-channel', "selected_rows"),],
+    [Input('all-button', 'n_clicks'),],
+    [State('datatable-interactivity-channel', "derived_virtual_data"),]
+)
+def select_all(n_clicks, selected_rows):
+    if selected_rows is None:
+        return [[]]
+    else:
+        return [[i for i in range(len(selected_rows))]]
+# end trash
+##
+
+eos_dropdown = dcc.Dropdown(id='eos-selected',
+                       options=[{'label':k,'value':k} for k in eoses],
+                       value=eoses[1])
 
 # Prep the EOS selector table
 columns = ls_grade.table_column_names.copy()
@@ -64,12 +93,21 @@ table = dash_table.DataTable(
     sorting_type="multi",
     selected_rows=[0,1,2,3])
 
+
+# The page structure
 layout = html.Div([
-    dcc.Graph(id="3d-graph"),
-    table,
-], className="two columns")
+    html.Div([
+        html.Div([dcc.Markdown("# EOS:")],className="two columns"),
+        html.Div([eos_dropdown],className="two columns"),
+    ],className="row"),
+    html.Div([
+        html.Div(dcc.Graph(id="3d-graph"),className="eight columns"),
+        html.Div(table,className="four columns"),
+    ],className="row"),
+    
+], className="row")
 
-
+# Callbacks
 @app.callback(
     Output("3d-graph", "figure"),
     [Input("select-table", "selected_row_ids")])
