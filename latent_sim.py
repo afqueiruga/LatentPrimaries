da3dbdd61dbdb66be6f780167c75f6248bd4ad18
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2019-08-30 10:45:47 -0700

    Assembling and iterating, but it's unstable

--- latent_sim.py
+++ latent_sim.py
@@ -253,11 +253,12 @@ class LatentSim():
         g = self.regvar("g",[0,-9.81])
         L = tf.norm(XA-XB,axis=-1)
         n = tf.einsum('ij,i->ij',(XB-XA),(1.0/L))
-        grav_forcing = tf.expand_dims(tf.einsum('j,ij->i',g, n),axis=-1 )
-        mflux = k_p * ( tf.einsum('ij,i->ij',(pB - pA),(1.0/L) )  - grav_forcing )
-        qflux = k_T * ( tf.einsum('ij,i->ij',(pB - pA),(1.0/L) ) )
         rhobar = (rhoB + rhoA)/2.0
         rho_hbar = (rho_hB + rho_hA)/2.0
+        grav_forcing = rhobar*tf.expand_dims(tf.einsum('j,ij->i',g, n),axis=-1 )
+        mflux = k_p * ( tf.einsum('ij,i->ij',(pB - pA),(1.0/L) )  - grav_forcing )
+        qflux = k_T * ( tf.einsum('ij,i->ij',(pB - pA),(1.0/L) ) )
+
         F = tf.concat([
             mflux,
             qflux + mflux*rho_hbar/rhobar,
